13a29f2af8940895fe3734aff65e2e8e472d3a52
sxurik <sxurik@gmail.com>
2021-05-12 14:29:05 +0300

    Use styled components for global styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 
+import GlobalStyle from './styles/GlobalStyle';
+
 export default function App() {
   return (
-    <div className="app">
-      <Router>
-        <Navigation />
-        <div className="content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/sign-up" component={SignUp} />
-          </Switch>
-        </div>
-        <div className="footer" />
-      </Router>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="app">
+        <Router>
+          <Navigation />
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+          <div className="footer" />
+        </Router>
+      </div>
+    </>
   );
 }
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,77 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    height: 100%;
+    margin: 0;
+    font-family: 'Comfortaa', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 16px;
+    background-color: var(--background);
+    color: var(--lavendar);
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  .app {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    flex: 1 0 auto;
+    margin-top: -93px;
+    padding-top: 93px;
+  }
+
+  .footer {
+    flex-shrink: 0;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+
+  :root {
+    /* --green: #6FD65E;
+    --green-light: #D5FFCE;
+    --pink: #EB5BEE;
+    --pink-light: #feccff;
+    --pink-lighter: #FFE5E5;
+    --blue: #1472FF;
+    --blue-light: #B6D3FF;
+    --gray-darker: #808080;
+    --gray-dark: #A7A7A7;
+    --gray: #E0E0E0;
+    --gray-light: #EDEDED;
+    --gray-lighter: #F3F3F3;
+    --gray-lighest: #F8F9FB;
+    --black: #444444; */
+    --purple: #250055;
+    --plum: #1D0535;
+    --periwinkle-base: 130, 135, 255;
+    --periwinkle: rgb(var(--periwinkle-base));
+    --lavendar-base: 248, 221, 255;
+    --lavendar: rgb(var(--lavendar-base));
+    --pink: #FF79ED;
+    --background: #23024B;
+    --background-dark: #1F043C;
+    --bubble-gum: #FF79ED;
+
+    --radius-default: 8px;
+  }
+`;
+
+export default GlobalStyle;
