b4e418f2673d00e77bdc5e198c3405a75d99bce8
sxurik <sxurik@gmail.com>
2021-05-14 17:13:56 +0300

    Add close button

---
+++ src/assets/CloseIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CloseIcon() {
+  return (
+    <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line y1="-0.5" x2="28.1439" y2="-0.5" transform="matrix(0.710634 0.703562 -0.710634 0.703562 1 1.19897)" />
+      <line y1="-0.5" x2="28.1439" y2="-0.5" transform="matrix(0.710634 -0.703562 0.710634 0.703562 1 20.801)" />
+    </svg>
+  );
+}
