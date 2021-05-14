b614cf2c5dd39276515414976d2e9eb20cd5b5df
sxurik <sxurik@gmail.com>
2021-05-14 11:04:31 +0300

    Add "Select Art" button

--- src/components/Mint/MintUpload.js
+++ src/components/Mint/MintUpload.js
@@ -16,9 +16,12 @@ const Container = styled('div')`
   }
 
   .select {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
     width: 100%;
     border-radius: var(--radius-default);
-    position: relative;
     // todo: use variables for 'rx='8' ry='8' (border-radius) stroke='%23${'F8DDFF'}' (lavendar)'
     background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%23${'F8DDFF'}' stroke-width='3' stroke-dasharray='10 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
 
@@ -27,6 +30,10 @@ const Container = styled('div')`
       display: block;
       padding-bottom: 100%;
     }
+  }
+
+  #file-uploader {
+    display: none;
   }
 
   .button-bottom {
@@ -53,7 +60,12 @@ export default function MintDescribe() {
   return (
     <Container>
       <HeadingText>Upload Art</HeadingText>
-      <div className="select" />
+      <div className="select">
+        <Button as="label" for="file-uploader" isSecondary>
+          Select Art
+        </Button>
+        <input id="file-uploader" type="file" />
+      </div>
       <div className="button-bottom">
         <Button isPrimary isLink>
           <Link to={`${match.path}/review`} onClick={() => {}}>
