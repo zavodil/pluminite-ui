07758538f06d65c84a7171e52ab5e003dbae4b15
sxurik <sxurik@gmail.com>
2021-05-14 12:57:59 +0300

    Process file select and file drop

--- src/components/common/ArtDropzone.js
+++ src/components/common/ArtDropzone.js
@@ -1,36 +1,97 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import { useDropzone } from 'react-dropzone';
 
 import Button from './Button';
 
 const StyledContainer = styled('div')`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  width: 100%;
-  border-radius: var(--radius-default);
-  // todo: use variables for 'rx='8' ry='8' (border-radius) stroke='%23${'F8DDFF'}' (lavendar)'
-  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%23${'F8DDFF'}' stroke-width='3' stroke-dasharray='10 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+  .square-with-border {
+    padding: 3px;
+    border-radius: var(--radius-default);
+    // todo: use variables for 'rx='8' ry='8' (border-radius) stroke='%23${'F8DDFF'}' (lavendar)'
+    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='8' ry='8' stroke='%23${'F8DDFF'}' stroke-width='3' stroke-dasharray='10 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+    width: 100%;
 
-  :after {
-    content: '';
-    display: block;
-    padding-bottom: 100%;
+    :after {
+      content: '';
+      display: block;
+      padding-bottom: 100%;
+    }
   }
 
-  #file-uploader {
-    display: none;
+  img {
+    width: 100%;
+    border-radius: var(--radius-default);
+  }
+
+  .select {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .extra-text {
+    text-align: center;
+  }
+
+  .advice {
+    color: var(--periwinkle);
   }
 `;
 
-const ArtDropzone = () => (
-  <StyledContainer className="select">
-    <Button as="label" for="file-uploader" isSecondary>
-      Select Art
-    </Button>
-    <input id="file-uploader" type="file" />
-  </StyledContainer>
-);
+const ArtDropzone = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [imageDataUrl, setImageDataUrl] = useState(null);
+  const [isError, setIsError] = useState(false);
+  const [filename, setFilename] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    setIsLoading(true);
+    setIsError(false);
+
+    const file = acceptedFiles[0];
+    setFilename(file.name);
+
+    const reader = new FileReader();
+    reader.onabort = () => {
+      setIsLoading(false);
+    };
+    reader.onerror = () => {
+      setIsLoading(false);
+      setIsError(true);
+    };
+    reader.onload = () => {
+      setIsLoading(false);
+      setImageDataUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <StyledContainer>
+      {imageDataUrl ? (
+        <img src={imageDataUrl} alt="selected art" />
+      ) : (
+        <div className="select square-with-border" {...getRootProps()}>
+          {isDragActive ? 'Drop files here.' : <Button isSecondary>Select Art</Button>}
+          <input
+            {...getInputProps({
+              multiple: false,
+              accept: 'image/*',
+            })}
+          />
+        </div>
+      )}
+      {filename && <p className="extra-text">{filename}</p>}
+      {isLoading && <p className="extra-text">Loading Art...</p>}
+      {isError && <p className="extra-text">Something is wrong. Try again.</p>}
+      <p className="advice extra-text">We advise a 1:1 ratio. Max file size WIP.</p>
+    </StyledContainer>
+  );
+};
 
 export default ArtDropzone;
