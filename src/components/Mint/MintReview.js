754fcef3d3da34688f7d49b71edc03d5dd4e97b9
sxurik <sxurik@gmail.com>
2021-05-14 13:36:37 +0300

    Show art placeholder on mint review page

--- src/components/Mint/MintReview.js
+++ src/components/Mint/MintReview.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { HeadingText } from '../common/typography';
 import Button from '../common/Button';
+import ArtItem from '../common/ArtItem';
 
 const Container = styled('div')`
   max-width: 600px;
@@ -28,12 +30,14 @@ const Container = styled('div')`
   }
 `;
 
-export default function MintReview() {
+const MintReview = ({ imageDataUrl, bid }) => {
   const match = useRouteMatch();
 
   return (
     <Container>
       <HeadingText>Yay!</HeadingText>
+      <p>This is how your NFT will appear on the marketplace. You cannot remove an NFT once it is minted.</p>
+      <ArtItem dataUrl={imageDataUrl} bid={bid} />
       <div className="button-bottom">
         <Button isPrimary isLink>
           <Link to={`${match.path}/profile`} onClick={() => {}}>
@@ -43,4 +47,11 @@ export default function MintReview() {
       </div>
     </Container>
   );
-}
+};
+
+MintReview.propTypes = {
+  imageDataUrl: PropTypes.string,
+  bid: PropTypes.string,
+};
+
+export default MintReview;
