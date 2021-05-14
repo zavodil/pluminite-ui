31b7f735df93ede4f5072bdbceb2d7cb9673b388
sxurik <sxurik@gmail.com>
2021-05-14 17:14:15 +0300

    Add custom message component

---
+++ src/components/common/messages/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ReactChildrenTypeRequired } from '../../../types/ReactChildrenTypes';
+
+const Message = ({ children }) => <div>{children}</div>;
+
+Message.propTypes = {
+  children: ReactChildrenTypeRequired,
+};
+
+export default Message;
