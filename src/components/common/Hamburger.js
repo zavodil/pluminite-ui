5eced3c9c4645d2fafdbccaa7d493190856860e3
sxurik <sxurik@gmail.com>
2021-05-14 18:46:18 +0300

    Add Hamburger component

---
+++ src/components/common/Hamburger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+
+  .bar {
+    position: relative;
+    display: block;
+    width: 30px;
+    height: 2px;
+    margin: 5px 0;
+    background-color: var(--lavendar);
+    border-radius: 3px;
+  }
+`;
+
+const Hamburger = () => (
+  <StyledContainer>
+    <span className="bar" />
+    <span className="bar" />
+  </StyledContainer>
+);
+
+export default Hamburger;
