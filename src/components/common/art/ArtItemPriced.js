b620803d5c894efb3f91d0674939a4b9088e33b6
sxurik <sxurik@gmail.com>
2021-05-15 14:52:17 +0300

    Split ArtItem component into editable and priced components

---
+++ src/components/common/art/ArtItemPriced.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ArtItem from './ArtItem';
+
+// todo: remove for production
+const placeholderBid = 55;
+
+const ArtItemPriced = ({ bid, bidAvailable, ...props }) => (
+  <ArtItem buttonText={`Bid {${bid}}Ⓝ`} isButtonDisabled={!bidAvailable} {...props} />
+);
+
+ArtItemPriced.propTypes = {
+  bid: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  bidAvailable: PropTypes.bool,
+};
+
+ArtItemPriced.defaultProps = {
+  // todo: remove for production
+  bid: placeholderBid,
+  bidAvailable: true,
+};
+
+export default ArtItemPriced;
---
+++ src/components/common/art/index.js
@@ -0,0 +1,5 @@
+import ArtItem from './ArtItem';
+import ArtItemPriced from './ArtItemPriced';
+import ArtItemEditable from './ArtItemEditable';
+
+export { ArtItem, ArtItemPriced, ArtItemEditable };
