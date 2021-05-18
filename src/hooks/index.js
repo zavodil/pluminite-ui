f8e2fd52edf24964461b77722e1fb2fd63d11fd0
sxurik <sxurik@gmail.com>
2021-05-18 16:58:39 +0300

    Create useQuery hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,6 @@ import useDetectClickOutside from './useDetectClickOutside';
 import useDocumentTitle from './useDocumentTitle';
 import useDebounce from './useDebounce';
 import withUSDs from './withUSDs';
+import useQuery from './useQuery';
 
-export { useDetectClickOutside, useDocumentTitle, useDebounce, withUSDs };
+export { useDetectClickOutside, useDocumentTitle, useDebounce, withUSDs, useQuery };
