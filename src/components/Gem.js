10545411de34c777e91794a34f051099afec5ae3
sxurik <sxurik@gmail.com>
2021-05-18 12:35:03 +0300

    Add top part of gem view page

--- src/components/Gem.js
+++ src/components/Gem.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { StickedToBottom } from './common/layout';
 import Balance from './NavigationComponents/Balance';
 import Button from './common/Button';
+import { TitleText } from './common/typography';
 
 const Container = styled('div')`
   display: flex;
@@ -13,6 +14,18 @@ const Container = styled('div')`
   max-width: 767px;
   padding: 100px 28px 120px;
 
+  .gem-title {
+    margin-bottom: 5px;
+  }
+
+  .users {
+    color: rgba(var(--lavendar-base), 0.7);
+
+    p {
+      margin: 0 0 5px;
+    }
+  }
+
   @media (min-width: 767px) {
     margin: 0 auto;
     align-items: center;
@@ -20,11 +33,15 @@ const Container = styled('div')`
 `;
 
 export default function Gem() {
-  const { gemId } = useParams();
+  // const { gemId } = useParams();
 
   return (
     <Container>
-      <h1>Art Title</h1>
+      <TitleText className="gem-title">Art Title</TitleText>
+      <div className="users">
+        <p>by bluesygma.near</p>
+        <p>owned by bluesygma.near</p>
+      </div>
     </Container>
   );
 }
