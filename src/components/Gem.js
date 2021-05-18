8939070afa8ea37b69ed936f3ef2c7c1c97edf94
sxurik <sxurik@gmail.com>
2021-05-18 13:38:10 +0300

    Use bottom part of gem page

--- src/components/Gem.js
+++ src/components/Gem.js
@@ -7,12 +7,11 @@ import Balance from './NavigationComponents/Balance';
 import Button from './common/Button';
 import { TitleText } from './common/typography';
 import { Tabs } from './common/tabs';
-import { ArtItemEditable } from './common/art';
 
 const Container = styled('div')`
   display: flex;
   flex-direction: column;
-  min-height: calc(100% - 90px);
+  min-height: calc(100% - 173px);
   max-width: 767px;
   padding: 100px 28px 120px;
 
@@ -37,9 +36,47 @@ const Container = styled('div')`
     margin-bottom: 40px;
   }
 
+  .bid {
+    width: 100%;
+
+    &-top {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+
   @media (min-width: 767px) {
     margin: 0 auto;
     align-items: center;
+  }
+`;
+
+const StyledBid = styled('div')`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .bid-top {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+
+  .bid-title {
+    font-size: 20px;
+    margin: 0 0 10px;
+  }
+
+  .bid-user {
+    color: rgba(var(--lavendar-base), 0.7);
+    margin: 0;
+  }
+
+  .bid-button {
+    width: 100%;
   }
 `;
 
@@ -63,6 +100,20 @@ export default function Gem() {
           { title: 'Royalties' },
         ]}
       />
+      <StickedToBottom isSecondary>
+        <StyledBid className="bid">
+          <div className="bid-top">
+            <div className="bid-description">
+              <p className="bid-title">Starting Bid</p>
+              <p className="bid-user">by bluesygma.near</p>
+            </div>
+            <Balance className="bid-sum" precision={0} />
+          </div>
+          <Button className="bid-button" isPrimary>
+            Bid 5Ⓝ on Gem
+          </Button>
+        </StyledBid>
+      </StickedToBottom>
     </Container>
   );
 }
