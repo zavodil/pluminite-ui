3480be1c81388c0ba8f3c8ee1724098b0b9c865e
sxurik <sxurik@gmail.com>
2021-05-19 12:21:16 +0300

    Use input sign component in input with aside text component

--- src/components/common/forms/InputSignAside.js
+++ src/components/common/forms/InputSignAside.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import { hideArrowsForNumberInput } from '../../../styles/mixins';
 import { SmallText } from '../typography';
+import InputSign from './InputSign';
 
 const StyledContainer = styled('div')`
   display: flex;
   flex-direction: column;
   margin-bottom: 50px;
+
+  .form-group {
+    margin-bottom: 0;
+  }
 
   label {
     line-height: 24px;
@@ -17,21 +21,6 @@ const StyledContainer = styled('div')`
 
   .small-text {
     margin-bottom: 16px;
-  }
-
-  input {
-    ${hideArrowsForNumberInput};
-
-    height: 55px;
-    width: 135px;
-    padding: 16px 40px 16px 14px;
-    border: none;
-    border-bottom: var(--lavendar) 1px solid;
-    background-color: rgba(var(--periwinkle-base), 0.2);
-    outline: none;
-    font-size: 16px;
-    color: #ffffff;
-    text-align: end;
   }
 
   .aside-wrapper {
@@ -44,23 +33,6 @@ const StyledContainer = styled('div')`
     align-items: center;
     margin-left: 22px;
     color: var(--periwinkle);
-  }
-
-  .sign-wrapper {
-    position: relative;
-    width: fit-content;
-  }
-
-  .sign {
-    position: absolute;
-    top: 0;
-    right: 0;
-    padding: 16px 14px;
-    font-family: Comfortaa, 'sans-serif';
-    font-size: 18px;
-    line-height: 24px;
-    color: darkgray;
-    cursor: default;
   }
 `;
 
@@ -79,17 +51,14 @@ const InputSignAside = ({
       {labelText && <label>{labelText}</label>}
       {detailsText && <SmallText>{detailsText}</SmallText>}
       <div className="aside-wrapper">
-        <div className="sign-wrapper">
-          <input
-            type="number"
-            name={name}
-            required={isRequired}
-            autoComplete="off"
-            onChange={(e) => inputOnChange(e.target.value)}
-            {...rest}
-          />
-          <div className="sign">{sign}</div>
-        </div>
+        <InputSign
+          type="number"
+          sign={sign}
+          name={name}
+          required={isRequired}
+          inputOnChange={inputOnChange}
+          {...rest}
+        />
         {asideText && <div className="aside">{asideText}</div>}
       </div>
     </StyledContainer>
