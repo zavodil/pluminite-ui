66bc7c441466216f3a5ae866f897a482df808918
sxurik <sxurik@gmail.com>
2021-05-19 18:30:27 +0300

    Add isError modifier to small text component

--- src/components/common/typography/SmallText.js
+++ src/components/common/typography/SmallText.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { ReactChildrenTypeRequired } from '../../../types/ReactChildrenTypes';
 
 const StyledContainer = styled('div')`
-  color: var(--periwinkle);
+  color: ${({ isError }) => (isError ? 'var(--error)' : 'var(--periwinkle)')};
   font-size: 12px;
   line-height: 21px;
 `;
 
-const SmallText = ({ children }) => <StyledContainer className="small-text">{children}</StyledContainer>;
+const SmallText = ({ children, isError }) => (
+  <StyledContainer isError={isError} className="small-text">
+    {children}
+  </StyledContainer>
+);
 
 SmallText.propTypes = {
   children: ReactChildrenTypeRequired,
+  isError: PropTypes.bool,
 };
 
 export default SmallText;
