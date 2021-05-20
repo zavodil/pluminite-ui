36605c2bf19b9b8a05210cd89d2d892e369448ab
sxurik <sxurik@gmail.com>
2021-05-20 14:13:51 +0300

    Add small heading component

---
+++ src/components/common/typography/HeadingSmallText.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactChildrenTypeRequired } from '../../../types/ReactChildrenTypes';
+
+const StyledH3 = styled('h3')`
+  margin: 0;
+  font-family: var(--font-secondary);
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: normal;
+  text-transform: uppercase;
+  color: var(--bubble-gum);
+`;
+
+const HeadingSmallText = ({ children }) => <StyledH3 className="heading-small">{children}</StyledH3>;
+
+HeadingSmallText.propTypes = {
+  children: ReactChildrenTypeRequired,
+};
+
+export default HeadingSmallText;
