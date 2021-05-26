c1cbca413c334191e5fd9697882ffc765624b9b7
sxurik <sxurik@gmail.com>
2021-05-26 17:26:42 +0300

    Crop image to 1:1 aspect ratio

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -58,10 +58,14 @@ export const square = css`
   }
 
   img {
+    object-fit: cover;
+  }
+
+  img,
+  canvas {
     position: absolute;
     width: 100%;
     height: 100%;
-    object-fit: cover;
     border-radius: var(--radius-default);
   }
 `;
