765839648797dd8a07a9d916fdc953df53a94162
sxurik <sxurik@gmail.com>
2021-05-31 13:31:56 +0300

    Fix art item preview on gem page

--- src/components/pages/gem/Gem.js
+++ src/components/pages/gem/Gem.js
@@ -265,7 +265,7 @@ function Gem({ location: { prevPathname } }) {
         tabsArray={[
           {
             title: 'Preview',
-            content: <ArtItem gemId={gem?.token_id} dataUrl={gem?.metadata?.media} isFullScreenEnabled />,
+            content: <ArtItem gemId={gem?.token_id} dataUrl={gem?.metadata?.media} isFullScreenEnabled isFromIpfs />,
           },
           {
             title: 'Description',
