5b61d674a7d868da9352b504d08e3e8156740ddd
sxurik <sxurik@gmail.com>
2021-05-31 11:24:05 +0300

    Fetch image data from ipfs once hash is available

--- src/components/common/images/ImageFromIpfs.js
+++ src/components/common/images/ImageFromIpfs.js
@@ -9,13 +9,16 @@ import { getFileData } from '../../../apis';
 import { QUERY_KEYS } from '../../../constants';
 
 const ImageFromIpfs = ({ media, alt, forwardedRef }) => {
-  const { data: imageData } = useQuery([QUERY_KEYS.GET_IMAGE_DATA, media], () => getFileData(media), { retry: 1 });
+  const { data: imageData } = useQuery([QUERY_KEYS.GET_IMAGE_DATA, media], () => getFileData(media), {
+    retry: 1,
+    enabled: !!media,
+  });
 
   return <Image ref={forwardedRef} src={imageData} alt={alt} className="image" />;
 };
 
 ImageFromIpfs.propTypes = {
-  media: PropTypes.string.isRequired,
+  media: PropTypes.string,
   alt: PropTypes.string,
   forwardedRef: PropTypes.object,
 };
