044088b4440480421b9fb5acabb2f17578cf54db
sxurik <sxurik@gmail.com>
2021-06-01 10:40:21 +0300

    Add error fallback component

---
+++ src/components/ErrorFallback.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './common/Button';
+
+import { HeadingText } from './common/typography';
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 0 28px;
+  text-align: center;
+`;
+
+const ErrorFallback = () => (
+  <Container>
+    <HeadingText>
+      Sorry :( <br /> There was an unexpected error
+    </HeadingText>
+    <Button isPrimary>
+      <a href="/">Back home</a>
+    </Button>
+  </Container>
+);
+
+export default ErrorFallback;
