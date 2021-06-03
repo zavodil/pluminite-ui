f9c2163a3b0e4bd7852e51499072943603425d7c
sxurik <sxurik@gmail.com>
2021-06-03 17:16:31 +0300

    Configure for development

--- src/apis/pinata.js
+++ src/apis/pinata.js
@@ -2,8 +2,22 @@ import pinataSDK from '@pinata/sdk';
 
 import { APP } from '../constants';
 
+let pinataApiKey;
+let pinataApiSecret;
+let pinataHref;
+
+if (process.env.NODE_ENV !== 'production') {
+  pinataApiKey = APP.PINATA_API_KEY;
+  pinataApiSecret = APP.PINATA_API_SECRET;
+  pinataHref = 'https://gateway.pinata.cloud/ipfs';
+} else {
+  pinataApiKey = process.env.PINATA_API_KEY;
+  pinataApiSecret = process.env.PINATA_API_SECRET;
+  pinataHref = 'https://storage.pluminite.com/ipfs';
+}
+
 export const getFileData = async (hash) => {
-  return fetch(`https://storage.pluminite.com/ipfs/${hash}`)
+  return fetch(`${pinataHref}/${hash}`)
       .then(res => {
         if (!res.ok)
           return null;
@@ -19,7 +33,7 @@ export const getFileData = async (hash) => {
 };
 
 export const uploadFileData = async (fileData) => {
-  const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_API_SECRET);
+  const pinata = pinataSDK(pinataApiKey, pinataApiSecret);
   const metadata = {};
   const data = {
     file: fileData,
