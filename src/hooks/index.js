bc19d137d60ff99e1b9231614f03f89a92b51aed
sxurik <sxurik@gmail.com>
2021-06-03 19:32:43 +0300

    Add custom hook to filter queries getting nft-s

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,5 +4,15 @@ import useDebounce from './useDebounce';
 import withUSDs from './withUSDs';
 import useQuery from './useQuery';
 import useIsUnmounting from './useIsUnmounting';
+import { useInfiniteQueryGemsWithBlackList, useQueryGemsWithBlackList } from './useBlacklist';
 
-export { useDetectClickOutside, useDocumentTitle, useDebounce, withUSDs, useQuery, useIsUnmounting };
+export {
+  useDetectClickOutside,
+  useDocumentTitle,
+  useDebounce,
+  withUSDs,
+  useQuery,
+  useIsUnmounting,
+  useInfiniteQueryGemsWithBlackList,
+  useQueryGemsWithBlackList,
+};
---
+++ src/hooks/useBlacklist.js
@@ -0,0 +1,39 @@
+import { useInfiniteQuery, useQuery } from 'react-query';
+import { getBlacklistedTokens } from '../apis';
+import { QUERY_KEYS } from '../constants';
+
+export const useQueryGemsWithBlackList = (queryKeys, queryFn, options) => {
+  const { data: blacklistedTokens } = useQuery([QUERY_KEYS.BLACKLIST], () => getBlacklistedTokens(), {
+    staleTime: 1000 * 60 * 10,
+  });
+
+  return useQuery(queryKeys, queryFn, {
+    ...options,
+    enabled: options.options && !!blacklistedTokens,
+    select: (dataRaw) => {
+      if (dataRaw?.pages?.length) {
+        return dataRaw.pages.flat().filter(({ token_id }) => !blacklistedTokens.includes(token_id));
+      }
+
+      return [];
+    },
+  });
+};
+
+export const useInfiniteQueryGemsWithBlackList = (queryKeys, queryFn, options) => {
+  const { data: blacklistedTokens } = useQuery([QUERY_KEYS.BLACKLIST], () => getBlacklistedTokens(), {
+    staleTime: 1000 * 60 * 10,
+  });
+
+  return useInfiniteQuery(queryKeys, queryFn, {
+    ...options,
+    enabled: options.options && !!blacklistedTokens,
+    select: (dataRaw) => {
+      if (dataRaw?.pages?.length) {
+        return dataRaw.pages.flat().filter(({ token_id }) => !blacklistedTokens.includes(token_id));
+      }
+
+      return [];
+    },
+  });
+};
