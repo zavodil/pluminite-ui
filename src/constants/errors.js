e1abff76fb4596cce73b9b9440eebf89e9064261
sxurik <sxurik@gmail.com>
2021-06-10 13:48:25 +0300

    Create errors and redirects constants

---
+++ src/constants/errors.js
@@ -0,0 +1,7 @@
+export const WALLER_ERRORS = {
+  NOT_ENOUGH_BALANCE: 'NotEnoughBalance',
+};
+
+export const ERROR_REDIRECTS = {
+  [WALLER_ERRORS.NOT_ENOUGH_BALANCE]: '/not-enough-balance',
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import APP from './app';
 import QUERY_KEYS from './queryKeys';
 import STORAGE from './storage';
 import { PAYABLE_METHODS, PAYABLE_METHODS_DESCRIPTIONS, PAYABLE_METHODS_SUCCESS_MESSAGES } from './methods';
+import { WALLER_ERRORS, ERROR_REDIRECTS } from './errors';
 
 export {
   PROFILE,
@@ -12,4 +13,6 @@ export {
   PAYABLE_METHODS,
   PAYABLE_METHODS_DESCRIPTIONS,
   PAYABLE_METHODS_SUCCESS_MESSAGES,
+  WALLER_ERRORS,
+  ERROR_REDIRECTS,
 };
