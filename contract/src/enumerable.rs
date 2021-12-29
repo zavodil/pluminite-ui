cafe2590538e91ae59ff8d5d94f5d5e4fa933b28
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-12-29 15:20:33 +0200

    nft_tokens implemented according to the standard

--- contract/src/enumerable.rs
+++ contract/src/enumerable.rs
@@ -6,12 +6,12 @@ impl Contract {
     pub fn nft_tokens(
         &self,
         from_index: U64,
-        limit: U64,
+        limit: u64,
     ) -> Vec<JsonToken> {
         let mut tmp = vec![];
         let keys = self.token_metadata_by_id.keys_as_vector();
         let start = u64::from(from_index);
-        let end = min(start + u64::from(limit), keys.len());
+        let end = min(start + limit, keys.len());
         for i in start..end {
             tmp.push(self.nft_token(keys.get(i).unwrap()).unwrap());
         }
