9c94fe7e4a2bb5cf7f753b13cfaf71f54f1e5087
NinoLipartiia4ire <n.lipartiia@4irelabs.com>
2021-12-29 15:43:52 +0200

    Added a method nft_payout according to the standard

--- contract/src/nft_core.rs
+++ contract/src/nft_core.rs
@@ -16,6 +16,8 @@ pub trait NonFungibleTokenCore {
         memo: Option<String>,
     );
 
+    fn nft_payout(&self, token_id: String, balance: U128, max_len_payout: u32) -> Payout;
+
     fn nft_transfer_payout(
         &mut self,
         receiver_id: ValidAccountId,
@@ -125,6 +127,39 @@ impl NonFungibleTokenCore for Contract {
             );
         }
     }
+
+    fn nft_payout(&self, token_id: String, balance: U128, max_len_payout: u32) -> Payout {
+		let token = self.tokens_by_id.get(&token_id).expect("No token");
+
+        // compute payouts based on balance option
+        // adds in contract_royalty and computes previous owner royalty from remainder
+        let owner_id = token.owner_id;
+        let mut total_perpetual = 0;
+        let balance_u128 = u128::from(balance);
+		let mut payout: Payout = HashMap::new();
+		let royalty = token.royalty;
+
+		assert!(royalty.len() as u32 <= max_len_payout, "Market cannot payout to that many receivers");
+
+		for (k, v) in royalty.iter() {
+			let key = k.clone();
+			if key != owner_id {
+				payout.insert(key, royalty_to_payout(*v, balance_u128));
+				total_perpetual += *v;
+			}
+		}
+
+		// payout to contract owner - may be previous token owner, they get remainder of balance
+		if self.contract_royalty > 0 && self.owner_id != owner_id {
+			payout.insert(self.owner_id.clone(), royalty_to_payout(self.contract_royalty, balance_u128));
+			total_perpetual += self.contract_royalty;
+		}
+		assert!(total_perpetual <= MINTER_ROYALTY_CAP + CONTRACT_ROYALTY_CAP, "Royalties should not be more than caps");
+		// payout to previous owner
+		payout.insert(owner_id, royalty_to_payout(10000 - total_perpetual, balance_u128));
+
+		payout
+	}
 
     // CUSTOM - this method is included for marketplaces that respect royalties
     #[payable]
